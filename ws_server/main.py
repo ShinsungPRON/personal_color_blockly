66e94c421d5a8faad45688f1e754f2aa44e3d33f
flyahn06 <flyahn06@gmail.com>
2023-10-25 08:45:35 +0900

    퍼스널컬러 실행 방식 변경

--- ws_server/main.py
+++ ws_server/main.py
@@ -1,9 +1,16 @@
 import asyncio
 import subprocess
 from websockets.server import serve
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), "conf.conf"))
+
 
 async def echo(websocket):
-    subprocess.run(['python3.10', '/Users/flyahn06/code/PRON/personal_color/main.py'])
+    subprocess.run([config['DEFAULT']['py'], config['DEFAULT']['filepath']])
+
 
 async def main():
     async with serve(echo, "localhost", 8080):
